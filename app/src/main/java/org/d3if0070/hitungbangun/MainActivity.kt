ae682a8c6672dd443542f00cc0178b4984646297
Yusuf-Abidin-Nurrahman <yusufabidin02@gmail.com>
2022-03-29 22:06:19 +0700

    Menambah view binding

--- app/src/main/java/org/d3if0070/hitungbangun/MainActivity.kt
+++ app/src/main/java/org/d3if0070/hitungbangun/MainActivity.kt
@@ -2,10 +2,22 @@ package org.d3if0070.hitungbangun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.d3if0070.hitungbangun.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnHitung.setOnClickListener{ hitungPP()}
+    }
+
+    private fun hitungPP(){
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
