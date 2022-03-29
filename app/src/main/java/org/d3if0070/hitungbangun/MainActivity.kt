605fe8827d42798ed81e9062ea9de09662539430
Yusuf-Abidin-Nurrahman <yusufabidin02@gmail.com>
2022-03-30 02:35:28 +0700

    perubahan tampilan, validasi input, dan fungsi reset

--- app/src/main/java/org/d3if0070/hitungbangun/MainActivity.kt
+++ app/src/main/java/org/d3if0070/hitungbangun/MainActivity.kt
@@ -2,6 +2,8 @@ package org.d3if0070.hitungbangun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import org.d3if0070.hitungbangun.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,17 +16,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnHitung.setOnClickListener{ hitungPP()}
+        binding.btnReset.setOnClickListener { reset() }
     }
 
+
     private fun hitungPP(){
-        val panjang = binding.editTextpanjang.text.toString().toFloat()
-        val lebar = binding.editTextlebar.text.toString().toFloat()
-        val keliling = 2 * (panjang + lebar)
-        val luas = panjang * lebar
+
+        val panjang = binding.editTextpanjang.text.toString()
+        if(TextUtils.isEmpty(panjang)){
+            Toast.makeText(this, R.string.panjang_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val lebar = binding.editTextlebar.text.toString()
+        if (TextUtils.isEmpty(lebar)){
+            Toast.makeText(this, R.string.lebar_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val keliling = 2 * (panjang.toFloat() + lebar.toFloat())
+        val luas = panjang.toFloat() * lebar.toFloat()
 
         binding.hasilKeliling.text = getString(R.string.hasilKeliling, keliling)
         binding.hasilLuas.text = getString(R.string.hasilLuas, luas)
 
+    }
+
+    private fun reset(){
+        binding.editTextlebar.setText("")
+        binding.editTextpanjang.setText("")
     }
 
 
