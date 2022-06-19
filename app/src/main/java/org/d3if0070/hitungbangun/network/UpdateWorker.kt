8d2e6194750b51fc206db7e5a4149ea120d1d429
Yusuf-Abidin-Nurrahman <yusufabidin02@gmail.com>
2022-06-19 22:52:42 +0700

    Membuat notifikasi updater

--- app/src/main/java/org/d3if0070/hitungbangun/network/UpdateWorker.kt
+++ app/src/main/java/org/d3if0070/hitungbangun/network/UpdateWorker.kt
@@ -1,17 +1,42 @@
 package org.d3if0070.hitungbangun.network
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import org.d3if0070.hitungbangun.MainActivity
+import org.d3if0070.hitungbangun.R
 
 class UpdateWorker (
     context: Context,
     workerParams: WorkerParameters
 ) : Worker(context, workerParams){
 
+    private val notificationId = 44
+
     override fun doWork(): Result{
-        Log.d("Worker", "Menjalankan proses background..")
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, 0)
+
+        val build = NotificationCompat.Builder(applicationContext,
+            MainActivity.CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(applicationContext.getString(
+                R.string.notif_title
+            ))
+            .setContentText(applicationContext.getString(
+                R.string.notif_text
+            ))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        val manager = NotificationManagerCompat.from(applicationContext)
+        manager.notify(notificationId, build.build())
         return Result.success()
     }
 
